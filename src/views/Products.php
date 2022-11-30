4dc5f05e313cd11bb8bc98e8792841f242943c1c
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2022-11-30 12:42:51 +0200

    Created template for products section

---
+++ src/views/Products.php
@@ -0,0 +1,12 @@
+<section class="h-screen grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 text-gray-900">
+    <article class="card">
+        <img class="h-3/4 rounded-t-lg" src='https://picsum.photos/1350/1080' />
+        <div class="h-1/4 rounded-b-lg p-4">
+          <h2 class="mb-2">Cat#01</h2>
+          <p>0.01 ETH</p>  
+        </div>
+        <div class="card-popup flex flex-row justify-center text-white">
+            <p>Add to cart</p>
+        </div>
+    </article>
+</section>
