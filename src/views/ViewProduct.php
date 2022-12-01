2eebbbf54aa1359d70e1c342e9ac913701c896fa
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2022-12-01 12:10:12 +0200

    Created dao for product to read all from db

--- src/views/ViewProduct.php
+++ src/views/ViewProduct.php
@@ -1,6 +1,11 @@
 <?php
+include __DIR__ . './../config/DbConfig.php';
+include __DIR__ . './../data/ProductDAO.php';
+
 if (isset($_POST['view-product'])) {
     $product_id = $_POST['view-product'];
+    $product_dao = new ProductDAO();
+    $product_data = $product_dao->readByID(new DbConfig(), $product_id);
 }
 
 include __DIR__ . './../config/DbConfig.php';
