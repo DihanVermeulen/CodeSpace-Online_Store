ca2e4dfece3e7574c8d7944351b196362732a00c
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2022-12-01 20:18:43 +0200

    Refactored

--- src/data/ProductDAO.php
+++ src/data/ProductDAO.php
@@ -1,13 +1,15 @@
 <?php
 
-class ProductDAO {
+class ProductDAO
+{
     // ====================Read====================
 
     /**
      * Reads product by ID
      * @param DbConfig requires database config
      */
-    public function readByID($DbConfig, $id) {
+    public function readByID($DbConfig, $id)
+    {
         $db_connection = $DbConfig->connect();
 
         $statement = "SELECT * FROM products WHERE id = '$id'";
@@ -18,32 +20,30 @@ class ProductDAO {
             $products = $result->fetch_object();
 
             return $products;
-
         } else {
             die("Connection failed: " . $db_connection->error); //die function to close connection in case of error
         }
-        
     }
 
     /**
      * Reads all products
      * @param DbConfig requires database config
      */
-    public function readAll($DbConfig) {
+    public function readAll($DbConfig)
+    {
         $db_connection = $DbConfig->connect();
 
         $statement = "SELECT * FROM products";
 
         if ($result = $db_connection->query($statement)) {
-
             $db_connection->close();
-            $products = $result->fetch_object();
+            while ($row = $result->fetch_assoc()) {
+                $products[] = $row;
+            }
 
             return $products;
-
         } else {
             die("Connection failed: " . $db_connection->error); //die function to close connection in case of error
         }
-        
     }
 }
