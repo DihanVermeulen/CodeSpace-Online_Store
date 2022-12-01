44a8fa1e3962cfa254ce487f9a73153229497414
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2022-12-01 07:24:45 +0200

    Updated to loop through data and display cards

--- src/views/Products.php
+++ src/views/Products.php
@@ -1,12 +1,30 @@
-<section class="h-screen grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 text-gray-900">
-    <article class="card">
-        <img class="h-3/4 rounded-t-lg" src='https://picsum.photos/1350/1080' />
-        <div class="h-1/4 rounded-b-lg p-4">
-          <h2 class="mb-2">Cat#01</h2>
-          <p>0.01 ETH</p>  
-        </div>
-        <div class="card-popup flex flex-row justify-center text-white">
-            <p>Add to cart</p>
-        </div>
-    </article>
+<div class="text-white">
+
+    <?php
+    include __DIR__ . '/../data.php';
+    ?>
+</div>
+
+
+<section class="text-white">
+    <header class="flex flex-row justify-center">
+        <h1 class="text-2xl font-extrabold font-poppins">Pick. Choose. Buy.</h1>
+    </header>
+    <form method="POST">
+        <main class="h-screen grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 text-gray-900">
+            <?php
+            foreach ($data as $item) { ?>
+                <article class="card">
+                    <img class="h-3/4 rounded-t-lg inline-block" src='https://picsum.photos/1350/1080' />
+                    <div class="h-1/4 rounded-b-lg p-4">
+                        <h2 class="mb-2"><?php echo $item['title'] ?></h2>
+                        <p><?php echo $item['price'] ?> ETH</p>
+                    </div>
+                    <div class="card-popup flex flex-row justify-center text-white">
+                        <button type="submit" class="font-extrabold">Add to cart</button>
+                    </div>
+                </article>
+            <?php } ?>
+        </main>
+    </form>
 </section>
