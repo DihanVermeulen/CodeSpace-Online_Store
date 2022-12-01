8d0a6b808a26dbd91ee0922feae534204e7e32b8
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2022-12-01 11:37:17 +0200

    Updated

--- src/views/Products.php
+++ src/views/Products.php
@@ -6,24 +6,23 @@
 </div>
 
 
-<section class="text-white">
+<section class="text-white p-8">
     <header class="flex flex-row justify-center">
         <h1 class="text-2xl font-extrabold font-poppins">Pick. Choose. Buy.</h1>
     </header>
-    <form method="POST">
+    <form method="POST" action="<?php echo $_SERVER['PHP_SELF'] ?>">
         <main class="h-screen grid grid-cols-2 gap-8 md:grid-cols-3 lg:grid-cols-4 text-gray-900">
             <?php
             foreach ($data as $item) { ?>
-                <article class="card">
-                    <img class="h-3/4 rounded-t-lg inline-block" src='https://picsum.photos/1350/1080' />
-                    <div class="h-1/4 rounded-b-lg p-4">
-                        <h2 class="mb-2"><?php echo $item['title'] ?></h2>
-                        <p><?php echo $item['price'] ?> ETH</p>
-                    </div>
-                    <div class="card-popup flex flex-row justify-center text-white">
-                        <button type="submit" class="font-extrabold">Add to cart</button>
-                    </div>
-                </article>
+                <button value="<?php echo $item['id'] ?>" type="submit" name="view-product" class="z-10 font-medium bg-transparent rounded-lg hover:bg-gray-800 focus:bg-gray-800">
+                    <article class="card">
+                        <img class="h-3/4 rounded-t-lg inline-block" src='https://picsum.photos/1350/1080' />
+                        <div class="h-1/4 rounded-b-lg p-4">
+                            <h2 class="mb-2"><?php echo $item['title'] ?></h2>
+                            <p><?php echo $item['price'] ?> ETH</p>
+                        </div>
+                    </article>
+                </button>
             <?php } ?>
         </main>
     </form>
