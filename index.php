2596734d8ac9709185846d064893d852ed455e5c
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2022-12-01 17:31:53 +0200

    Added navigation menu for mobile

--- index.php
+++ index.php
@@ -19,47 +19,36 @@
 </head>
 
 <body>
-    <nav class="flex items-center bg-transparent border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
+    <nav class="inline-flex items-center bg-transparent border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
         <div class="container flex flex-wrap items-center justify-between mx-auto">
             <img src="public/assets/images/logo.png" class="h-6 mr-3 sm:h-9" alt="Flowbite Logo" />
-            <button data-collapse-toggle="navbar" type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar" aria-expanded="false">
+            <button data-drawer-target="drawer-navigation" data-drawer-show="drawer-navigation" aria-controls="drawer-navigation" type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-700 focus:bg-gray-700" aria-controls="navbar" aria-expanded="false">
                 <span class="sr-only">Open main menu</span>
                 <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                     <path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path>
                 </svg>
             </button>
 
-            <div class="hidden w-full md:block md:w-auto z-10" id="navbar">
-                <!-- <ul class="flex flex-col mt-20 absolute right-0 md:static lg:static bg-white md:bg-transparent md:mr-8 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700" id="tabs" data-tabs-toggle="#tab-content" role="tablist">
-                    <li>
-                        <a id="home-tab" data-tabs-target="#home" type="button" role="tab" aria-controls="home" aria-selected="false" class="block py-2 pl-3 pr-4 text-gray-500 md:text-white rounded md:border-0 hover:text-blue-300 md:p-0">
-                            <i class="fa-sharp fa-solid fa-house"></i>
-                            <span>Home</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a id="products-tab" data-tabs-target="#products" type="button" role="tab" aria-controls="products" aria-selected="false" class="block py-2 pl-3 pr-4 text-gray-500 md:text-gray-300 rounded md:border-0 hover:text-blue-300 md:p-0">
-                            <i class="fa-sharp fa-solid fa-bag-shopping"></i>
-                            <span>Products</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a id="login-tab" data-tabs-target="#login" role="tab" aria-controls="login" aria-selected="false" class="block py-2 pl-3 pr-4 text-gray-500 md:text-gray-300 rounded md:border-0 hover:text-blue-300 md:p-0">
-                            <i class="fa-sharp fa-solid fa-arrow-right-to-bracket"></i>
-                            <span>Login</span>
-                        </a>
-                    </li>
-                </ul> -->
+            <div class="hidden w-full md:block md:w-auto z-10">
                 <form method="POST">
-                    <ul class="flex flex-col mt-20 z-50 absolute right-0 md:static lg:static bg-white md:bg-transparent md:mr-8 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700" id="tabs" data-tabs-toggle="#tab-content" role="tablist">
-                        <li>
-                            <button type="submit" name="home-tab" class="block py-2 pl-3 pr-4 text-gray-500 md:text-white rounded md:border-0 hover:text-blue-300 md:p-0">Home</button>
+                    <ul class="flex flex-col justify-center mr-8 z-50 absolute right-0 md:static lg:static bg-white md:bg-transparent md:mr-8 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700" id="tabs" data-tabs-toggle="#tab-content" role="tablist">
+                        <li class="flex items-center">
+                            <button type="submit" name="home-tab" class="text-gray-500 hover:bg-gray-700 hover:text-white rounded-lg p-1 text-center inline-flex items-center">
+                                <i class="fa-solid fa-house icon"></i>Home</button>
                         </li>
-                        <li>
-                            <button type="submit" name="products-tab" class="block py-2 pl-3 pr-4 text-gray-500 md:text-white rounded md:border-0 hover:text-blue-300 md:p-0">Products</button>
+                        <li class="flex items-center">
+                            <button type="submit" name="products-tab" class="text-gray-500 hover:bg-gray-700 hover:text-white rounded-lg p-1 text-center inline-flex items-center">
+                                <i class="fa-solid fa-bag-shopping icon"></i>Products</button>
                         </li>
-                        <li>
-                            <button type="submit" name="login-tab" class="block py-2 pl-3 pr-4 text-gray-500 md:text-white rounded md:border-0 hover:text-blue-300 md:p-0">Login</button>
+                        <li class="flex items-center">
+                            <button type="submit" name="login-tab" class="text-gray-500 hover:bg-gray-700 hover:text-white rounded-lg p-1 text-center inline-flex items-center">
+                                <i class="fa-solid fa-right-to-bracket icon"></i>Login</button>
+                        </li>
+                        <li class="flex items-center">
+                            <button type="submit" class="text-gray-500 hover:bg-gray-700 hover:text-white rounded-lg p-1 relative text-center inline-flex items-center">
+                                <i class="fa-solid fa-cart-shopping icon"></i>
+                                Cart
+                            </button>
                         </li>
                     </ul>
                 </form>
@@ -67,6 +56,55 @@
 
         </div>
     </nav>
+
+    <!-- drawer component -->
+    <div id="drawer-navigation" class="fixed z-40 h-screen p-4 overflow-y-auto bg-white w-80 dark:bg-gray-800" tabindex="-1" aria-labelledby="drawer-navigation-label">
+        <h5 id="drawer-navigation-label" class="text-base font-semibold text-gray-500 uppercase dark:text-gray-400">Menu</h5>
+        <button type="button" data-drawer-dismiss="drawer-navigation" aria-controls="drawer-navigation" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 absolute top-2.5 right-2.5 inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white">
+            <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+            </svg>
+            <span class="sr-only">Close menu</span>
+        </button>
+        <div class="py-4 overflow-y-auto">
+            <ul class="space-y-2">
+                <li>
+                    <a href="#" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+                        <svg aria-hidden="true" class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+                        </svg>
+                        <span class="flex-1 ml-3 whitespace-nowrap">Home</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+                        <svg aria-hidden="true" class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z" clip-rule="evenodd"></path>
+                        </svg>
+                        <span class="flex-1 ml-3 whitespace-nowrap">Products</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+                        <svg aria-hidden="true" class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" d="M3 3a1 1 0 00-1 1v12a1 1 0 102 0V4a1 1 0 00-1-1zm10.293 9.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L14.586 9H7a1 1 0 100 2h7.586l-1.293 1.293z" clip-rule="evenodd"></path>
+                        </svg>
+                        <span class="flex-1 ml-3 whitespace-nowrap">Sign In</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+                        <svg aria-hidden="true" class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" d="M5 4a3 3 0 00-3 3v6a3 3 0 003 3h10a3 3 0 003-3V7a3 3 0 00-3-3H5zm-1 9v-1h5v2H5a1 1 0 01-1-1zm7 1h4a1 1 0 001-1v-1h-5v2zm0-4h5V8h-5v2zM9 8H4v2h5V8z" clip-rule="evenodd"></path>
+                        </svg>
+                        <span class="flex-1 ml-3 whitespace-nowrap">Sign Up</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+
 
     <div class="w-full h-full z-0" id="tab-content">
 
