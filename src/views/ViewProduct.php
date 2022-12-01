a235d1e777926200d4cd818ce115c7aff597a647
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2022-12-01 16:02:35 +0200

    Added section to view image

--- src/views/ViewProduct.php
+++ src/views/ViewProduct.php
@@ -6,9 +6,14 @@ if (isset($_POST['view-product'])) {
     $product_id = $_POST['view-product'];
     $product_dao = new ProductDAO();
     $product_data = $product_dao->readByID(new DbConfig(), $product_id);
+    print_r($product_data);
 }
-
-include __DIR__ . './../config/DbConfig.php';
-include __DIR__ . './../data/ProductDAO.php';
-
 ?>
+<section class="flex p-8">
+    <main class="w-1/4 mr-4">
+        <img class="w-full rounded-lg" src='<?php echo $product_data->product_image_location ?>' />
+    </main>
+    <aside class="w-full text-left">
+        <h1 class="text-4xl"><?php echo $product_data->product_name ?></h1>
+    </aside>
+</section>
