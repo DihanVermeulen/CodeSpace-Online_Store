05a2a045c15db0d63aa2ad7a6c5fd3138f064b68
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2022-12-02 15:54:48 +0200

    Checks to see if item exists and adds to that quantity

--- src/views/Cart.php
+++ src/views/Cart.php
@@ -1,11 +1,31 @@
 <?php
 if (isset($_POST['add-to-cart'])) {
+    $quantity = $_POST['quantity'];
+    $id = $_POST['id'];
+    $cart_item_exists = false;
 
-    array_push($_SESSION['cart'], array(
-        "id" => $_POST['id'],
-        "quantity" => $_POST['quantity']
-    ));
+    foreach ($_SESSION['cart'] as $cart_item) {
+        if ($cart_item['id'] == $id) {
+            $quantity += $cart_item['quantity'];
+            $cart_item_exists = true;
+            array_push($_SESSION['cart'], array(
+                "id" => $id,
+                "quantity" => $quantity
+            ));
+            echo "items exists";
+            echo $quantity;
+            break;
+        }
+    }
 
-    print_r($_SESSION['cart']);
+    if(!$cart_item_exists) {
+        echo "item does not exist";
+        array_push($_SESSION['cart'], array(
+            "id" => $id,
+            "quantity" => $quantity
+        ));
+    }
+
 }
 ?>
+
