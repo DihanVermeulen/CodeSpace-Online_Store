aada1b693145287af772ae0a3fb1b173a7ba0a2c
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2022-12-02 08:08:05 +0200

    Changed to SVG icons and updated classes

--- index.php
+++ index.php
@@ -24,9 +24,11 @@ if (!isset($_SESSION['cart'])) {
 </head>
 
 <body>
+
+    <!-- Navbar -->
     <nav class="inline-flex items-center w-full bg-transparent border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
         <div class="container flex flex-wrap items-center justify-between mx-auto">
-            <img src="public/assets/images/logo.png" class="h-6 mr-3 sm:h-9" alt="Flowbite Logo" />
+            <img src="public/assets/images/logo.png" class="h-6 mr-3 sm:h-9" alt="Logo" />
             <button data-drawer-target="drawer-navigation" data-drawer-show="drawer-navigation" aria-controls="drawer-navigation" type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-700 focus:bg-gray-700" aria-controls="navbar" aria-expanded="false">
                 <span class="sr-only">Open main menu</span>
                 <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
@@ -34,25 +36,41 @@ if (!isset($_SESSION['cart'])) {
                 </svg>
             </button>
 
-            <div class="hidden w-full md:block md:w-auto z-10">
-                <form method="POST">
-                    <ul class="flex flex-col justify-center mr-8 z-50 absolute right-0 md:static lg:static bg-white md:bg-transparent md:mr-8 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700" id="tabs" data-tabs-toggle="#tab-content" role="tablist">
+            <div class="w-full md:block md:w-auto z-10">
+                <form method="POST" class="flex items-center">
+                    <ul class="hidden md:flex justify-center mr-8 z-50 absolute right-0 bg-transparent md:mr-8 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0" id="tabs" data-tabs-toggle="#tab-content" role="tablist">
                         <li class="flex items-center">
                             <button type="submit" name="home-tab" class="text-gray-500 hover:text-white rounded-lg p-1 text-center inline-flex items-center">
-                                <i class="fa-solid fa-house icon"></i>Home</button>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                                    <path d="M11.47 3.84a.75.75 0 011.06 0l8.69 8.69a.75.75 0 101.06-1.06l-8.689-8.69a2.25 2.25 0 00-3.182 0l-8.69 8.69a.75.75 0 001.061 1.06l8.69-8.69z" />
+                                    <path d="M12 5.432l8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 01-.75-.75v-4.5a.75.75 0 00-.75-.75h-3a.75.75 0 00-.75.75V21a.75.75 0 01-.75.75H5.625a1.875 1.875 0 01-1.875-1.875v-6.198a2.29 2.29 0 00.091-.086L12 5.43z" />
+                                </svg>
+
+                                <span>Home</span>
+                            </button>
                         </li>
                         <li class="flex items-center">
-                            <button type="submit" name="products-tab" class="text-gray-500 hover:text-white rounded-lg p-1 text-center inline-flex items-center">
-                                <i class="fa-solid fa-bag-shopping icon"></i>Products</button>
+                            <button type="submit" name="products-tab" class="text-gray-500 hover:text-white rounded-lg text-center inline-flex items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                                    <path fill-rule="evenodd" d="M7.5 6v.75H5.513c-.96 0-1.764.724-1.865 1.679l-1.263 12A1.875 1.875 0 004.25 22.5h15.5a1.875 1.875 0 001.865-2.071l-1.263-12a1.875 1.875 0 00-1.865-1.679H16.5V6a4.5 4.5 0 10-9 0zM12 3a3 3 0 00-3 3v.75h6V6a3 3 0 00-3-3zm-3 8.25a3 3 0 106 0v-.75a.75.75 0 011.5 0v.75a4.5 4.5 0 11-9 0v-.75a.75.75 0 011.5 0v.75z" clip-rule="evenodd" />
+                                </svg>
+                                <span>Products</span>
+                            </button>
                         </li>
                         <li class="flex items-center">
-                            <button type="submit" name="login-tab" class="text-gray-500 hover:text-white rounded-lg p-1 text-center inline-flex items-center">
-                                <i class="fa-solid fa-right-to-bracket icon"></i>Login</button>
+                            <button type="submit" name="login-tab" class="text-gray-500 hover:text-white rounded-lg text-center inline-flex items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                                    <path fill-rule="evenodd" d="M7.5 3.75A1.5 1.5 0 006 5.25v13.5a1.5 1.5 0 001.5 1.5h6a1.5 1.5 0 001.5-1.5V15a.75.75 0 011.5 0v3.75a3 3 0 01-3 3h-6a3 3 0 01-3-3V5.25a3 3 0 013-3h6a3 3 0 013 3V9A.75.75 0 0115 9V5.25a1.5 1.5 0 00-1.5-1.5h-6zm10.72 4.72a.75.75 0 011.06 0l3 3a.75.75 0 010 1.06l-3 3a.75.75 0 11-1.06-1.06l1.72-1.72H9a.75.75 0 010-1.5h10.94l-1.72-1.72a.75.75 0 010-1.06z" clip-rule="evenodd" />
+                                </svg>
+                                <span>Login</span>
+                            </button>
                         </li>
                         <li class="flex items-center">
-                            <button type="submit" class="text-gray-500 hover:text-white rounded-lg p-1 relative text-center inline-flex items-center">
-                                <i class="fa-solid fa-cart-shopping icon"></i>
-                                Cart
+                            <button type="submit" class="text-gray-500 hover:text-white rounded-lg relative text-center inline-flex items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                                    <path d="M2.25 2.25a.75.75 0 000 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 00-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 000-1.5H5.378A2.25 2.25 0 017.5 15h11.218a.75.75 0 00.674-.421 60.358 60.358 0 002.96-7.228.75.75 0 00-.525-.965A60.864 60.864 0 005.68 4.509l-.232-.867A1.875 1.875 0 003.636 2.25H2.25zM3.75 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zM16.5 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0z" />
+                                </svg>
+                                <span>Cart</span>
                             </button>
                         </li>
                     </ul>
@@ -62,8 +80,8 @@ if (!isset($_SESSION['cart'])) {
         </div>
     </nav>
 
-    <!-- drawer component -->
-    <div id="drawer-navigation" class="sm:hidden fixed z-40 h-screen p-4 overflow-y-auto bg-white w-80 dark:bg-gray-800" tabindex="-1" aria-labelledby="drawer-navigation-label">
+    <!-- Drawer -->
+    <div id="drawer-navigation" class="md:hidden fixed z-40 h-screen p-4 overflow-y-auto bg-white w-80 dark:bg-gray-800" tabindex="-1" aria-labelledby="drawer-navigation-label">
         <h5 id="drawer-navigation-label" class="text-base font-semibold text-gray-500 uppercase dark:text-gray-400">Menu</h5>
         <button type="button" data-drawer-dismiss="drawer-navigation" aria-controls="drawer-navigation" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 absolute top-2.5 right-2.5 inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white">
             <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
@@ -109,24 +127,21 @@ if (!isset($_SESSION['cart'])) {
         </div>
     </div>
 
+    <!-- Tab content -->
+    <?php
+    if (isset($_POST['home-tab'])) {
+        include_once('src/views/Home.php');
+    } else if (isset($_POST['products-tab'])) {
+        include_once('src/views/Products.php');
+    } else if (isset($_POST['login-tab'])) {
+        include_once('src/views/Login.php');
+    } else if (isset($_POST['view-product'])) {
+        include_once('src/views/ViewProduct.php');
+    } else {
+        include_once('src/views/Home.php');
+    }
+    ?>
 
-
-    <div class="w-full h-full z-0" id="tab-content">
-
-        <?php
-        if (isset($_POST['home-tab'])) {
-            include_once('src/views/Home.php');
-        } else if (isset($_POST['products-tab'])) {
-            include_once('src/views/Products.php');
-        } else if (isset($_POST['login-tab'])) {
-            include_once('src/views/Login.php');
-        } else if (isset($_POST['view-product'])) {
-            include_once('src/views/ViewProduct.php');
-        } else {
-            include_once('src/views/Home.php');
-        }
-        ?>
-    </div>
 </body>
 
 </html>
