8d2bee8c1efd02c1c4f3e3175f58d045da8cbbd9
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2022-12-03 12:06:20 +0200

    Created new google maps embed

--- src/views/Contact.php
+++ src/views/Contact.php
@@ -1,5 +1,12 @@
-<div style="width: 100%">
-    <iframe width="100%" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Beach%20Rd,%20Strand,%20Cape%20Town+(Whisker%20Exchange)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">
-        <a href="https://www.maps.ie/distance-area-calculator.html">area maps</a>
-    </iframe>
-</div>
+<section class="flex flex-col p-8">
+    <div class="w-1/2 mt-20">
+        <iframe width="100%" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Beach%20Rd,%20Strand,%20Cape%20Town+(Whisker%20Exchange)&amp;t=h&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">
+            <a href="https://www.maps.ie/distance-area-calculator.html">
+                measure area map
+            </a>
+        </iframe>
+    </div>
+    <article>
+
+    </article>
+</section>
