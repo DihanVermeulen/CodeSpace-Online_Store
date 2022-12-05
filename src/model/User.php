3d9b413bbfae5c11c1c4d3b04532d5f287b1d074
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2022-12-05 14:29:47 +0200

    Create function to validate user information

--- src/model/User.php
+++ src/model/User.php
@@ -12,33 +12,59 @@ class User
 
     public function __construct($name, $surname, $email, $password)
     {
-        $this->$name = $name;
-        $this->$surname = $surname;
-        $this->$email = $email;
-        $this->$password = $password;
+        $this->name = $name;
+        $this->surname = $surname;
+        $this->email = $email;
+        $this->password = $password;
     }
 
     // ====================Methods====================
 
+    /**
+     * Encrypts password
+     */
     private function hashPassword($password)
     {
         $hash_password = password_hash($password, PASSWORD_BCRYPT);
         return $hash_password;
     }
 
+    /**
+     * Verifies hashed password
+     */
     private function verifyPassword($hash)
     {
         $is_password_correct = password_verify($hash, PASSWORD_BCRYPT);
         return $is_password_correct;
     }
 
-    public function createUser() {
+    /**
+     * Creates user array
+     * @return self
+     */
+    public function createUser()
+    {
         return array(
-            "user_name"=> $this->getName(),
-            "user_surname"=> $this->getSurname(),
+            "user_name" => $this->getName(),
+            "user_surname" => $this->getSurname(),
             "user_email"=> $this->getEmail(),
-            "user_password"=> $this->hashPassword($this->getPassword())
+            "user_password" => $this->getPassword()
         );
+    }
+
+    public static function validateUserData($user)
+    {
+        $user_is_valid = false;
+
+        $password_regex = "/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/";
+
+        // Uses regex and filter_var to validate
+        if(preg_match($password_regex, $user['user_password']) && filter_var($user['user_email'], FILTER_VALIDATE_EMAIL)) {
+            $user_is_valid = true;
+        }
+
+        // Returns true or false
+        return $user_is_valid;
     }
 
     // ====================Get and Set====================
@@ -106,7 +132,7 @@ class User
     /**
      * Get the value of password
      */
-    private function getPassword()
+    public function getPassword()
     {
         return $this->password;
     }
