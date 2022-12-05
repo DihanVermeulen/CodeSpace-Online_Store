f25d71a3ad47c754ed299cffac8d3e8c1f8507ad
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2022-12-05 16:55:35 +0200

    Added section to show featured products

--- src/views/Home.php
+++ src/views/Home.php
@@ -24,6 +24,25 @@ $product_data = $product_dao->readAll(new DbConfig());
         <div class="w-full flex justify-end">
             <img src='./../../public//assets//images//cat_paw_hovering_card.png' class=' right-0 hero-section-image' />
         </div>
-    </section>  
+    </section>
 
-  
+    <!-- Featured products section -->
+    <section class="h-screen flex flex-col justify-center items-center p-8">
+        <h2 class="text-5xl sm:text-6xl md:text-6xl lg:text-6xl xl:text-7xl mb-8">Featured products</h2>
+        <div class="flex space-x-4 sm:space-x-8 md:space-x-8 lg:space-x-8 xl:space-x-8">
+            <?php
+            for ($index = 0; $index < 3; $index++) { ?>
+                <button value="<?php echo $product_data[$index]['id'] ?>" type="submit" name="view-product" class="z-10 font-medium bg-transparent hover:drop-shadow-lg rounded-lg hover:bg-gray-800 focus:bg-indigo-300">
+                    <article class="card">
+                        <img class="h-2/3 rounded-t-lg object-cover" src='<?php echo $product_data[$index]['product_image_location'] ?>' />
+                        <div class="h-1/3 rounded-b-lg p-4">
+                            <h2 class="mb-2 text-2xl text-gray-500"><?php echo $product_data[$index]['product_name'] ?></h2>
+                            <p class="text-xl text-gray-300"><?php echo $product_data[$index]['product_price'] ?> ETH</p>
+                        </div>
+                    </article>
+                </button>
+            <?php } ?>
+        </div>
+    </section>
+
+   
