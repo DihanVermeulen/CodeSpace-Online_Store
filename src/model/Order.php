fc9d0a71f347d2e3be61eefa2b332a12829df467
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2022-12-07 10:06:45 +0200

    Created class with constructor method and variables

---
+++ src/model/Order.php
@@ -0,0 +1,20 @@
+<?php 
+
+class Order {
+
+    // ====================Variables====================
+
+    private $product_id;
+    private $user_id;
+    private $quantity;
+    private $created_at;
+
+    public function __construct($product_id, $user_id, $quantity, $created_at)
+    {
+        $this->product_id = $product_id;
+        $this->user_id = $user_id;
+        $this->quantity = $quantity;
+        $this->created_at = $created_at;
+    }
+
+}
