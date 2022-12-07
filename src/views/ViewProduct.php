5a61e1d0b01db47ccafefd2c311d568b7f770156
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2022-12-07 13:35:02 +0200

    If user is not logged in then add to cart button will be disabled

--- src/views/ViewProduct.php
+++ src/views/ViewProduct.php
@@ -24,7 +24,7 @@ if (isset($_POST['view-product'])) {
                 <span class="text-gray-300 font-light mb-8">R<?php echo $product_data->product_price * 22399.03 ?></span>
             </div>
             <form method="POST" class="flex items-end">
-                <button type="submit" name="add-to-cart" class="w-28 mr-8 h-12 inline-flex text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 px-5 py-2.5 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-base px-1 text-center">Add to cart</button>
+                <button type="submit" name="add-to-cart" <?php $is_logged_in ? "" : "disabled" ?> class="w-28 mr-8 h-12 inline-flex text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 px-5 py-2.5 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-base px-1 text-center">Add to cart</button>
                 <div class="flex flex-col justify-items-start">
                     <label for="quantity" class="block mb-2 text-sm font-medium">QTY</label>
                     <input type="number" name="quantity" id="quantity" min='1' max='9' value='1' class="bg-gray-50 w-1/3 text-gray-800 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-3">
