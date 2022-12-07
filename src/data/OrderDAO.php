90fd7d22d90bdd7829ea0efa6e48a87618c54515
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2022-12-07 14:01:12 +0200

    Posts order to database

--- src/data/OrderDAO.php
+++ src/data/OrderDAO.php
@@ -1,5 +1,36 @@
 <?php
 
 class OrderDAO {
-    
+    //====================Post====================
+
+    public static function postOrderToDb($DbConfig, $order)
+    {
+        $db_connection = $DbConfig->connect();
+
+        $statement = "INSERT INTO orders VALUES(null, ?, ?, ?, ?)";
+
+        echo "product id: " . $order->getProduct_ID();
+        echo "user id: " . $order->getUser_id();
+        echo "quantity: " . $order->getQuantity();
+        echo "created at: " . $order->getCreated_at();
+
+        $product_id = $order->getProduct_ID();
+        $user_id = $order->getUser_id();
+        $quantity = $order->getQuantity();
+        $created_at = $order->getCreated_at();
+
+        // Check connection
+        if ($db_connection->connect_error) {
+            die("Connection failed: " . $db_connection->connect_error);
+        }
+        try {
+
+            $stmt = $db_connection->prepare($statement);
+            $stmt->bind_param('ssss', $product_id, $user_id, $quantity, $created_at);
+            
+            $stmt->execute();
+        } catch(Error $err) {
+            echo "Unknown error occured when trying to place order.";
+        }
+    }
 }
