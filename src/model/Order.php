1fe57eb664b3a06df1545f54ca8627d90c299fae
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2022-12-07 13:39:34 +0200

    Inserted getters and setters for each variable

--- src/model/Order.php
+++ src/model/Order.php
@@ -10,7 +10,7 @@ class Order {
     private $created_at;
  
     // ====================Constructor====================
-
+    
     public function __construct($product_id, $user_id, $quantity, $created_at)
     {
         $this->product_id = $product_id;
@@ -18,5 +18,88 @@ class Order {
         $this->quantity = $quantity;
         $this->created_at = $created_at;
     }
+    
+    // ====================Constructor====================
 
+
+
+    /**
+     * Get the value of product_id
+     */ 
+    public function getProduct_id()
+    {
+        return $this->product_id;
+    }
+
+    /**
+     * Set the value of product_id
+     *
+     * @return  self
+     */ 
+    public function setProduct_id($product_id)
+    {
+        $this->product_id = $product_id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of user_id
+     */ 
+    public function getUser_id()
+    {
+        return $this->user_id;
+    }
+
+    /**
+     * Set the value of user_id
+     *
+     * @return  self
+     */ 
+    public function setUser_id($user_id)
+    {
+        $this->user_id = $user_id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of quantity
+     */ 
+    public function getQuantity()
+    {
+        return $this->quantity;
+    }
+
+    /**
+     * Set the value of quantity
+     *
+     * @return  self
+     */ 
+    public function setQuantity($quantity)
+    {
+        $this->quantity = $quantity;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of created_at
+     */ 
+    public function getCreated_at()
+    {
+        return $this->created_at;
+    }
+
+    /**
+     * Set the value of created_at
+     *
+     * @return  self
+     */ 
+    public function setCreated_at($created_at)
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
 }
