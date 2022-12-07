0ab4dc2f8995dd20c4f5d2634aa069dd3bfa247f
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2022-12-07 14:01:42 +0200

    Loop through cart items and adds them to database

--- src/views/Cart.php
+++ src/views/Cart.php
@@ -74,5 +74,16 @@ if (isset($_POST['add-to-cart'])) {
                 <path fill-rule="evenodd" d="M7.5 6v.75H5.513c-.96 0-1.764.724-1.865 1.679l-1.263 12A1.875 1.875 0 004.25 22.5h15.5a1.875 1.875 0 001.865-2.071l-1.263-12a1.875 1.875 0 00-1.865-1.679H16.5V6a4.5 4.5 0 10-9 0zM12 3a3 3 0 00-3 3v.75h6V6a3 3 0 00-3-3zm-3 8.25a3 3 0 106 0v-.75a.75.75 0 011.5 0v.75a4.5 4.5 0 11-9 0v-.75a.75.75 0 011.5 0v.75z" clip-rule="evenodd" />
             </svg>
         </button>
-    </div>
+    </form>
 </section>
+
+<?php
+if (isset($_POST['checkout'])) {
+    foreach ($_SESSION['cart'] as $item) {
+        $order = new Order($item['id'], $_COOKIE['logged_in_as'], $item['quantity'], date('d-m-y h:i:s'));
+        var_dump($order);
+        echo "<br>";
+        OrderDAO::postOrderToDb(new DbConfig(), $order);
+    }
+}
+?>
