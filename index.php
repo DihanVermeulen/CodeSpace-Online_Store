9dc6c52bc7816585e3a497a55f35e417ca0d4962
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2022-12-08 16:17:39 +0200

    Removed echo statements

--- index.php
+++ index.php
@@ -6,11 +6,8 @@ error_reporting(E_ALL);
 ob_start(); // Initiate the output buffer
 $is_logged_in = false;
 if (isset($_COOKIE['logged_in_as'])) {
-    echo "User is logged in";
     $is_logged_in = true;
     session_start();
-} else {
-    echo "User is not logged in";
 }
 
 if (!isset($_SESSION['cart'])) {
