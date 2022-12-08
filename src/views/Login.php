192494e50cc5f9772b43c4f2568a5f7257123b16
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2022-12-08 16:47:02 +0200

    Added autofocus attribute to email field

--- src/views/Login.php
+++ src/views/Login.php
@@ -11,7 +11,7 @@ include __DIR__ . './../config/DbConfig.php';
     </form>
     <form method="POST" class="w-96">
         <div class="relative mb-4">
-            <input type="text" id="email" name="email-input" class="block rounded-t-lg px-2.5 pb-2.5 pt-3 w-full text-lg text-gray-900 bg-gray-50 dark:bg-gray-700 border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
+            <input type="text" id="email" autofocus name="email-input" class="block rounded-t-lg px-2.5 pb-2.5 pt-3 w-full text-lg text-gray-900 bg-gray-50 dark:bg-gray-700 border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
             <label for="email" class="absolute text-md text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-6 z-10 origin-[0] left-2.5 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email</label>
         </div>
         <div class="relative mb-4">
