38ddc1b8af9b98ed251ca73dd6b7d0d3c8faa520
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2022-12-08 17:31:46 +0200

    Cursor pointer on back to top button

--- src/views/Home.php
+++ src/views/Home.php
@@ -139,7 +139,7 @@ $product_data = $product_dao->readAll(new DbConfig());
     </section>
 
     <div class="flex flex-col items-center">
-        <a onclick="scrollToTop();return false" class="flex flex-col items-center"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#fff" class="w-6 h-6">
+        <a onclick="scrollToTop();return false" class="flex flex-col items-center cursor-pointer"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#fff" class="w-6 h-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M12 19.5v-15m0 0l-6.75 6.75M12 4.5l6.75 6.75" />
             </svg><span>Back to top</span></a>
     </div>
