9abb1143d288126a6dfc7e68113f484af5708128
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2022-12-09 13:55:51 +0200

    Checks if user is logged in and disables button accordingly

--- src/views/ViewProduct.php
+++ src/views/ViewProduct.php
@@ -7,7 +7,15 @@ if (isset($_POST['view-product'])) {
     $product_dao = new ProductDAO();
     $product_data = $product_dao->readByID(new DbConfig(), $product_id);
 }
+
+// Checks if user is logged in and disables add to cart button
+$is_logged_in = false;
+if (isset($_COOKIE['logged_in_as'])) {
+    $is_logged_in = true;
+    echo "user is logged in";
+}
 ?>
+
 <section class="flex p-8">
     <main class="w-1/4 mr-4">
         <img class="w-full rounded-lg" src='<?php echo $product_data->product_image_location ?>' />
@@ -24,7 +32,7 @@ if (isset($_POST['view-product'])) {
                 <span class="text-gray-300 font-light mb-8">R<?php echo $product_data->product_price * 22399.03 ?></span>
             </div>
             <form method="POST" class="flex items-end">
-                <button type="submit" name="add-to-cart" <?php $is_logged_in ? "" : "disabled" ?> class="w-28 mr-8 h-12 inline-flex text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 px-5 py-2.5 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-base px-1 text-center">Add to cart</button>
+                <button type="submit" <?php echo $is_logged_in ? "" : "disabled" ?> name="add-to-cart" <?php $is_logged_in ? "" : "disabled" ?> class="w-28 mr-8 h-12 inline-flex text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 px-5 py-2.5 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-base px-1 text-center">Add to cart</button>
                 <div class="flex flex-col justify-items-start">
                     <label for="quantity" class="block mb-2 text-sm font-medium">QTY</label>
                     <input type="number" name="quantity" id="quantity" min='1' max='9' value='1' class="bg-gray-50 w-1/3 text-gray-800 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-3">
@@ -34,6 +42,9 @@ if (isset($_POST['view-product'])) {
                     <input name="image" type="hidden" value="<?php echo $product_data->product_image_location ?>">
                 </div>
             </form>
+            <?php if (!$is_logged_in) { ?>
+                <p>Please log in to add items to cart</p>
+            <?php } ?>
         </div>
     </aside>
 </section>
