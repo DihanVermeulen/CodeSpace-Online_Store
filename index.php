ec2c5f37476bade73a278991d8b097efdc85a36c
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2022-12-09 10:46:37 +0200

    Updated flowbite cdn

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ if (!isset($_SESSION['cart'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://unpkg.com/flowbite@1.5.4/dist/flowbite.min.css" />
-    <script src="https://unpkg.com/flowbite@1.5.4/dist/flowbite.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.5.5/flowbite.js"></script>
     <link rel="stylesheet" href="public/styles/styles.css">
     <title>Whisker Exchange</title>
 </head>
