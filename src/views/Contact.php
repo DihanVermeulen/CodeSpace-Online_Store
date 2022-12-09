9458a351d25bb4da33ee72483c0f988b12a89778
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2022-12-09 13:55:16 +0200

    Changed google maps background colour

--- src/views/Contact.php
+++ src/views/Contact.php
@@ -1,5 +1,5 @@
 <section class="flex p-8">
-    <div class="w-1/2 mt-20 bg-gray-300">
+    <div class="w-1/2 mt-20 bg-gray-600">
         <iframe width="100%" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Beach%20Rd,%20Strand,%20Cape%20Town+(Whisker%20Exchange)&amp;t=h&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">
             <a href="https://www.maps.ie/distance-area-calculator.html">
                 measure area map
