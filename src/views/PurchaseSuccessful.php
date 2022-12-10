94ebec2df4b6faa48cd33b8f10a7ac5a53bb176c
Dihan_Vermeulen <dihan.vermeulen12@gmail.com>
2022-12-10 10:50:21 +0200

    Created view page to show successful purchase

---
+++ src/views/PurchaseSuccessful.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Purchase Complete</title>
+</head>
+<body>
+<section class="w-screen h-screen">
+    <h1 class="text-8xl">Thank you!</h1>
+    <p class="text-6xl">Your purchase has been received</p>
+    <form method="POST">
+        <button type="submit" name="products-tab" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Go back to shopping</button>
+    </form>
+</section>
+</body>
+</html>
